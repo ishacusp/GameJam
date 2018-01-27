2da292203ce4f19d0d5be70d12db4d3b1613dd5b
Abraham Gellis <aagellis@gmail.com>
2018-01-27 14:21:17 -0500

    Pausing when cursor becomes unlocked

---
+++ GGJ2018/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour {
+	public GameObject GameUIRoot;
+	public GameObject PauseUIRoot;
+
+	public Button ResumeButton;
+
+	void Start() {
+		Unpause ();
+
+		ResumeButton.onClick.AddListener (Unpause);
+	}
+
+	void Update () {
+		if (Cursor.lockState != CursorLockMode.Locked)
+			Pause ();
+	}
+
+	void Pause() {
+		PauseUIRoot.gameObject.SetActive (true);
+		GameUIRoot.gameObject.SetActive (false);
+
+		Time.timeScale = 0f;
+
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	void Unpause() {
+		PauseUIRoot.gameObject.SetActive (false);
+		GameUIRoot.gameObject.SetActive (true);
+
+		Time.timeScale = 1f;
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+}
