641408be1d92e083924fb9dd541de490dca18e2a
Abraham Gellis <aagellis@gmail.com>
2018-01-28 15:48:56 -0500

    Title

---
+++ GGJ2018/Assets/Scripts/TitleScreenControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenControl : MonoBehaviour {
+	public GameObject Instructions;
+
+	void Start() {
+		Instructions.gameObject.SetActive (false);
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {	
+			if (Instructions.gameObject.activeSelf)
+				StartGame ();
+			else
+				Instructions.gameObject.SetActive (true);
+		}
+
+		if (Input.GetMouseButton (1)) {
+			Instructions.gameObject.SetActive (false);
+		}
+	}
+
+	void StartGame() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
