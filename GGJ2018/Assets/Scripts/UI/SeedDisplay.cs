9ee8a4ef9f2eb4578f28731a9fe5e80752c9d277
Abraham Gellis <aagellis@gmail.com>
2018-01-28 12:26:17 -0500

    Animated seed count display

--- GGJ2018/Assets/Scripts/UI/SeedDisplay.cs
+++ GGJ2018/Assets/Scripts/UI/SeedDisplay.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
-//TODO rework into something more attractive?
 public class SeedDisplay : MonoBehaviour {
+	public RectTransform Graphic;
 	public Text Display;
 
+	private int prevSeedCount;
+
 	void OnEnable() {
+		prevSeedCount = SeedControl.SceneInstance.Seeds;
+
 		SeedControl.SceneInstance.SeedCountUpdated += UpdateDisplay;
+
 		UpdateDisplay ();
 	}
 
@@ -17,6 +23,21 @@ public class SeedDisplay : MonoBehaviour {
 	}
 
 	void UpdateDisplay() {
-		Display.text = string.Format ("Seeds Remaining: {0}", SeedControl.SceneInstance.Seeds);
+		int seeds = SeedControl.SceneInstance.Seeds;
+
+		Display.text = seeds.ToString();
+
+		Graphic.transform.DOKill (true);
+
+		if (seeds > prevSeedCount) {
+			Graphic.transform.DOPunchScale (Graphic.transform.localScale * 0.4f, 0.5f, 15);
+		} else if (seeds < prevSeedCount) {
+			if (seeds > 0)
+				Graphic.transform.DOPunchRotation (Vector3.forward * 20f, 0.3f, 10);
+			else
+				Graphic.transform.DOPunchRotation (Vector3.forward * 40f, 0.5f, 15);
+		}
+
+		prevSeedCount = seeds;
 	}
 }
