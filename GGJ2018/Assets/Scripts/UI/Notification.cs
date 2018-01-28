4d9bee491e33e44f0f489f5615fc8c44e4831fe5
Abraham Gellis <aagellis@gmail.com>
2018-01-27 20:58:38 -0500

    Notifications resume fading after unpausing

--- GGJ2018/Assets/Scripts/UI/Notification.cs
+++ GGJ2018/Assets/Scripts/UI/Notification.cs
@@ -8,7 +8,25 @@ public class Notification : MonoBehaviour {
 	public float DisplayTime = 2f;
 	public float FadeTime = 0.25f;
 
-	IEnumerator Start () {
+	Coroutine fadeRoutine;
+
+	void Start() {
+		StartCoroutine (FadeAfterDelay ());
+	}
+
+	void OnEnable() {
+		PauseControl.SceneInstance.OnUnpaused += OnUnpaused;
+	}
+
+	void OnDisable() {
+		PauseControl.SceneInstance.OnUnpaused -= OnUnpaused;
+	}
+
+	void OnUnpaused() {
+		StartCoroutine (FadeAfterDelay ());
+	}
+
+	IEnumerator FadeAfterDelay () {
 		yield return new WaitForSeconds (DisplayTime);
 
 		for (float t = 0f; t < FadeTime; t += Time.deltaTime) {
